6f605116568b6aa7292709ffdc2d86ff93e41b0a
itechdom <oalghnmi@gmail.com>
2025-08-21 17:34:56 +0200

    fix delete note issue

--- src/components/MindMap.tsx
+++ src/components/MindMap.tsx
@@ -163,6 +163,7 @@ const MindMap: React.FC<MindMapProps> = ({
       }
       if(e.key === 'Backspace') {
         //check if editing is on, if so don't delete note
+        console.log('editing', editingState);
         if(editingState) {
           return;
         }
