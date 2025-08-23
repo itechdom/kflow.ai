afdb84e8dbad99290bd706761d101207ee19eb19
itechdom <oalghnmi@gmail.com>
2025-08-23 17:58:27 +0200

    generate uuid

--- src/features/notes/noteSlice.ts
+++ src/features/notes/noteSlice.ts
@@ -1,14 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { Note } from './Note';
-
-// Generate a simple UUID v4-like string
-function generateUUID(): string {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    const r = Math.random() * 16 | 0;
-    const v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
+import { generateUUID } from '../../utils';
 
 interface NoteState {
   notes: Note[];
